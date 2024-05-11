13f171a0290035c7bd935ea84c44a31c02029eda
ary <mariwi1503@gmail.com>
2024-05-11 10:33:42 +0700

    fix reset password

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -6,7 +6,7 @@ router.post("/auth/register", authController.register);
 router.post("/auth/verify", authController.verifyEmail);
 router.post("/auth/login", authController.login);
 router.post("/auth/resend-otp", authController.resendOtp);
-router.get("/auth/new-password", authController.login);
+router.get("/auth/new-password", authController.forgotPassword);
 router.post("/auth/reset-password", middleware.user, authController.login);
 router.put("/auth/logout", middleware.user, authController.logout);
 
